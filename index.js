29f0503c1061f306ecfb072ddf0f3f3465a27a6f
Karnik0909 <gandhikarnik0909@gmail.com>
2023-06-01 00:01:22 +0530

    File updated

---
+++ index.js
@@ -0,0 +1,17 @@
+const menuToggle = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', function() {
+  menu.classList.toggle('menu-open');
+});
+
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
