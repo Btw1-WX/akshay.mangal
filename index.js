205e383c34e0f23eb5decda303a098f18e1af4dd
Arham-1003 <gandhiarham1003@gmail.com>
2023-07-28 18:27:35 +0530

    blog page changes

--- index.js
+++ index.js
@@ -16,45 +16,6 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// JavaScript
-document.addEventListener("DOMContentLoaded", function () {
-  // Display Twitter blogs by default
-  showBlogs("twitter-blogs");
-
-  // Add click event listeners to blog bar links
-  const blogBarLinks = document.querySelectorAll(".blog-bar span");
-  for (const link of blogBarLinks) {
-      link.addEventListener("click", function () {
-          // Get the data-type attribute value (e.g., "twitter-blogs" or "linkedin-blogs")
-          const blogType = this.getAttribute("data-type");
-          showBlogs(blogType);
-      });
-  }
-});
-
-function showBlogs(blogType) {
-  // Hide all blog containers
-  const blogContainers = document.querySelectorAll(".blogs > div");
-  for (const container of blogContainers) {
-      container.style.display = "none";
-  }
-
-  // Show the selected blog container
-  const selectedContainer = document.getElementById(blogType);
-  if (selectedContainer) {
-      selectedContainer.style.display = "grid";
-  }
-
-  // Update the active class in the blog bar
-  const blogBarLinks = document.querySelectorAll(".blog-bar > span");
-  for (const link of blogBarLinks) {
-      link.classList.remove("active");
-  }
-  const activeLink = document.querySelector(`.blog-bar > span[data-type="${blogType}"]`);
-  if (activeLink) {
-      activeLink.classList.add("active");
-  }
-}
 
 
 
